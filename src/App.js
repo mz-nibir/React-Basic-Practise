cd7b4f734b3eddf28183c5d2d4240f227b2b670e
Nibir <mz.nibir@gmail.com>
2021-01-31 22:47:32 +0600

    basic feature and syntax

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Book from './components/book';
+import Person from './components/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//functional Componets
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello world!</h1>
+//       <Person />
+//     </div>
+//   );
+// }
+
+//class components
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      books: [
+        { bookName: "1984", writter: "orwell" },
+        { bookName: "The Da", writter: "Dan Brown" },
+        { bookName: "Alchemist", writter: "Paulo" }
+      ],
+      anotherProperty: "some text"
+    }
+  }
+  changeBookstate = (newpara) => {
+    this.setState({
+      books: [
+        { bookName: newpara, writter: "Nibir" },
+        { bookName: "The Da", writter: "Dan Brown" },
+        { bookName: "meta", writter: "franz" }
+      ]
+
+    });
+  }
+  changeWithinputState = (e) => {
+    this.setState({
+      books: [
+        { bookName: e.target.value, writter: "Nibir" },
+        { bookName: "The Da", writter: "Dan Brown" },
+        { bookName: "meta", writter: "franz" }
+      ]
+
+    });
+
+  }
+
+  render() {
+    const style = {
+      border: "1px solid red",
+      borderRadius: "5px",
+      backgroundColor: "black",
+      color: "blue"
+    }
+    return (
+      <div className="App">
+        <h1 style={style}>Book List</h1>
+        <button onClick={this.changeBookstate.bind(this, "new Book")} >Change State</button>
+        <input type="text" onChange={this.changeWithinputState} />
+        <Book
+          bookName={this.state.books[0].bookName}
+          writter={this.state.books[0].writter}
+          inputName={this.changeWithinputState} />
+        <Book
+          bookName={this.state.books[1].bookName}
+          writter={this.state.books[1].writter} />
+        <Book
+          bookName={this.state.books[2].bookName}
+          writter={this.state.books[2].writter}
+          change={() => this.changeBookstate("84")}
+        />
+
+      </div>
+    );
+
+  }
 }
+
+
+
 
 export default App;
---
+++ src/components/book.js
@@ -0,0 +1,14 @@
+import '../stylesheets/book.css'
+const Book = (props) => {
+    return (
+        <div className="book">
+            <h3 onClick={props.change}>Book:{props.bookName}</h3>
+            <h4>Writter: {props.writter}</h4>
+            <input type="text" onChange={props.inputName} value={props.bookName} />
+        </div>
+
+
+
+    );
+}
+export default Book;
